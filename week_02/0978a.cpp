d3f4a10ad30f860d8e016e0138e21463cf14812e
masarikq <masarfit@mail.ru>
2026-09-17 13:57:09 +0300

    2 задача

--- week_02/0978a.cpp
+++ week_02/0978a.cpp
@@ -3,12 +3,34 @@
 using namespace std;
 
 int main() {
-    vector<int> nams;
-    vector<int> nams2;
-    int nam;
+
     int x;
     cin >> x;
 
+    vector<int> a(x);
+    for (int i = 0; i<x; i++) {
+        cin >> a[i];
+    }
 
+    vector<int> res;
+
+    for (int i = 0; i < x; i ++) {
+        bool c = false;
+        for (int j = i + 1; j < x; j++) {
+            if (a[i] == a[j]) {
+                c = true;
+                break;
+            }
+        }
+        if (c == false) {
+            res.push_back(a[i]);
+        }
+    }
+
+    cout<<res.size()<<endl;
+
+    for (int a : res) {
+        cout<<a<<"  ";
+    }
     return 0;
 }
